7245c391b00e4d325a1d426f756fdc7228d207ed
Ivan <trajk.ivan@gmail.com>
2020-12-25 14:47:22 +0100

    small change at auth

--- client/src/components/auth/Auth.js
+++ client/src/components/auth/Auth.js
@@ -4,8 +4,22 @@ import {logIn, logOut} from '../../actions/index';
 
 
 import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
 class Auth extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: ""
+    }
+  }
+  handleInput = (event) => {
+    event.preventDefault();
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
 
   renderAuth = (status) => {
     if (status) {
@@ -18,7 +32,24 @@ class Auth extends Component {
     } else {
       return (
         < React.Fragment >
-          <h4>Here will be the inputs</h4>
+          <TextField
+            name="email"
+            type="email"
+            id="outlined-basic"
+            label="e-mail"
+            variant="outlined"
+            autoComplete="off"
+            onChange={(e) => this.handleInput(e)}
+          />
+          <TextField
+            id="outlined-password-input"
+            name="password"
+            label="Password"
+            type="password"
+            autoComplete="current-password"
+            variant="outlined"
+            onChange={(e) => this.handleInput(e)}
+          />
           <Button variant='contained' color='secondary' onClick={this.props.logIn}>Log In</Button>
         </React.Fragment >
       )
@@ -27,6 +58,7 @@ class Auth extends Component {
 
 
   render() {
+    console.log(this.state)
     return (
       <div>
         {this.renderAuth(this.props.auth.isLoggedIn)}
